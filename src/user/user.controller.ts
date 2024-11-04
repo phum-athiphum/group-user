e98eb93bf7b1e97e5ef38f31b80130c0409278f2
Phum Athiphum <>
2024-11-04 11:10:31 +0700

    finish group user logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get()
+  @HttpCode(200)
+  async getUserByDepartment() {
+    try {
+      const data = await this.userService.userBydepartment();
+      return data;
+    } catch (error) {
+      console.error('Error in getUserByDepartment:', error);
+      throw new HttpException(
+        'Failed to group user data',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { groupByDepartment } from 'src/utils/user';
+import { DepartmentSummary } from 'src/types/user';
 
+const dummyUrl = 'https://dummyjson.com/users';
 @Injectable()
-export class UserService {}
+export class UserService {
+  async userBydepartment(): Promise<DepartmentSummary[]> {
+    try {
+      const response = await axios.get(dummyUrl);
+      let groupUser: DepartmentSummary[] = [];
+      if (response.data.users && response.data.users.length > 0) {
+        groupUser = groupByDepartment(response.data.users);
+      }
+      return groupUser;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw new Error('Error fetch dummy user');
+    }
+  }
+}
