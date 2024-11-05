1b30617541a3a2dcb06178207db2ec1ab0e3da39
Phum Athiphum <>
2024-11-05 09:56:51 +0700

    add unit test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register()],
       providers: [UserService],
     }).compile();
 
